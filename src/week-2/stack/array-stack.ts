078e847c4a5e53d9f48697439decc85eab4305f6
interist32-mi\interist32 <interist32@gmail.com>
2020-11-17 21:09:33 -0800

    Make ArrayStack implementation more Java alike.

--- src/week-2/stack/array-stack.ts
+++ src/week-2/stack/array-stack.ts
@@ -1,14 +1,28 @@
 import {Stack} from './stack.interface';
 
+
+/**
+ * In Javascript array are dynamic, but for the purity of experiment I treat it
+ * like in Java or C++ imitating the process of allocation memory.
+ */
 export class ArrayStack<T> implements Stack<T> {
-  private readonly array = [];
+  private array = Array.from({length: 1}, () => null);
+  private nextPosition = 0;
 
   push(item: T) {
-    this.array.push(item);
+    this.array[this.nextPosition++] = item;
+    if (this.nextPosition === this.array.length) {
+      // double the length once the size limit is reached
+      this.array =
+          [...this.array, Array.from({length: this.array.length}, () => null)];
+    }
   }
 
   pop(): T {
-    return this.array.pop();
+    this.nextPosition--;
+    const item = this.array[this.nextPosition];
+    this.array = this.array.slice(0, this.nextPosition);
+    return item;
   }
 
   isEmpty(): boolean {
