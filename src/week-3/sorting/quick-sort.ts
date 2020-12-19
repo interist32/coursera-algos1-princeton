50fd11f5d72c081762bd80e098ea323fe48e85bc
interist32-mi\interist32 <interist32@gmail.com>
2020-12-18 17:16:26 -0800

    Fix bug.

--- src/week-3/sorting/quick-sort.ts
+++ src/week-3/sorting/quick-sort.ts
@@ -3,21 +3,20 @@ import {shuffle} from '../../week-2/shuffling/shuffling';
 
 
 export function quickSort<T>(arr: T[]) {
-  sort(arr, 0, arr.length);
+  sort(arr, 0, arr.length - 1);
   return arr;
 }
 
 export function quickSortWithShuffling<T>(arr: T[]) {
   shuffle(arr);
-  sort(arr, 0, arr.length);
+  sort(arr, 0, arr.length - 1);
   return arr;
 }
 
 function sort<T>(arr: T[], lo: number, hi: number) {
-  debugger;
   if (lo < hi) {
     const p = partition(arr, lo, hi);
-    sort(arr, lo, p);
+    sort(arr, lo, p - 1);
     sort(arr, p + 1, hi);
   }
 }
@@ -25,7 +24,7 @@ function sort<T>(arr: T[], lo: number, hi: number) {
 function partition<T>(arr: T[], lo: number, hi: number) {
   const pivot = arr[lo];
   let i = lo;
-  let j = hi;
+  let j = hi + 1;
 
   while (i < j) {
     do {
